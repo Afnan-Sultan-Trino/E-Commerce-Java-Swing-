5a08e7871d4e763697a15699bd0c465a667c0234
Emad <emadharun.eh@gmail.com>
2024-09-15 00:38:17 +0600

    UserManager Interface

--- interfaces/entities/IUserManager.java
+++ interfaces/entities/IUserManager.java
@@ -1,5 +1,6 @@
 package interfaces.entities;
 
 public interface IUserManager {
-    
+    public boolean userExists(String email);
+    public boolean validCredentials(String email, String password);
 }
