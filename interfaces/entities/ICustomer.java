4aa402e309348a849d15468a89c5ff3f5416f1e8
Emad <emadharun.eh@gmail.com>
2024-09-15 23:58:49 +0600

    new abstract methods

--- interfaces/entities/ICustomer.java
+++ interfaces/entities/ICustomer.java
@@ -1,5 +1,10 @@
 package interfaces.entities;
 
 public interface ICustomer {
-    
+    public void setContactNo(String contactNo);
+    public void setGender(String gender);
+    public void setAddress(String address);
+    public String getContactNo();
+    public String getGender();
+    public String getAddress();
 }
