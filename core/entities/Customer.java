f00b3598dc4ae884c87818c23c0112cf0aaa53d8
Emad <emadharun.eh@gmail.com>
2024-09-15 13:52:09 +0600

    New attributes and methods

--- core/entities/Customer.java
+++ core/entities/Customer.java
@@ -1,7 +1,32 @@
 package core.entities;
 
 public class Customer extends User {
-    public Customer(String name, String email, String password, String role) {
+
+    private String gender;
+    private String contactNo;
+    private String address;
+
+    public Customer(String name, String email, String password, String role, String gender, String contactNo, String address) {
         super(name, email, password, role);
+        this.gender = gender;
+        setContactNo(contactNo);
+        this.address = address;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+        // Do Number Validation
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
