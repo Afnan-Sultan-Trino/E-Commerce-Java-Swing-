eb31735cf49ad6040364bd6951ee9c4baf4a6d8e
Emad <emadharun.eh@gmail.com>
2024-09-23 22:27:42 +0600

    Optimised Imports

--- gui/dashboards/CustomerDashboard.java
+++ gui/dashboards/CustomerDashboard.java
@@ -2,11 +2,8 @@ package gui.dashboards;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import core.entities.Product;
